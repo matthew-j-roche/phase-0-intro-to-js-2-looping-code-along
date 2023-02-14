10cca1403b1c55b9ae8d22f6faee204e6ddf895c
matt roche <matt.roche@gmail.com>
2023-02-14 08:05:50 -0600

    intro to looping

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"];
+const writeCards = (arr,event) => {  
+    const messages = []
+    for(let i = 0; i < arr.length; i++) {                
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }          
+           return messages;
+    
+   
+
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown--);
+}
+  console.log(countdown);
